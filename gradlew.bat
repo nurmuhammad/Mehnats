eb8b5b1deac0b661d10f148a356ee0159e6ccd05
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2021-12-04 18:43:53 +0500

    mehnats commit

--- gradlew.bat
+++ gradlew.bat
@@ -14,12 +14,14 @@
 @rem limitations under the License.
 @rem
 
+
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
 @rem
 @rem ##########################################################################
+
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
