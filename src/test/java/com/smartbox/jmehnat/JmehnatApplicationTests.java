0cf67872b959d07ad9df21d53bf42715f16893da
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2021-12-04 16:33:45 +0500

    jMehnat commit with telegram bot library

---
+++ src/test/java/com/smartbox/jmehnat/JmehnatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbox.jmehnat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmehnatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
